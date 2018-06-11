4107473cbe9a9abaadb48e4be60aa3648cd4d479
Heikki <heikki.rantala@aalto.fi>
2018-06-11 12:12:29 +0300

    luovutetut alueet hierarchy

--- hierarchy_to_place_ontology.py
+++ hierarchy_to_place_ontology.py
@@ -26,7 +26,7 @@ def first_level_broader(g):
         g.add((URIRef(row['relPlace']['value']), namespace.SKOS.broader, URIRef(row['broaderPlace']['value'])))
 
 
-def ares_given_to_sssr(g):
+def ares_given_to_ussr(g):
     q = '''
             PREFIX yso: <http://www.yso.fi/onto/yso/>
             PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
@@ -47,6 +47,29 @@ def ares_given_to_sssr(g):
     g.add((rel.p_luovutetut_alueet, namespace.SKOS.prefLabel, Literal("Neuvostoliitolle luovutetut alueet")))
 
     for row in response.json()['results']['bindings']:
+        if  "Neuvostoliitolle luovutetun alueen" in row['note']['value'] or \
+                "luovutetulle alueelle" in row['note']['value']:
+            g.add((URIRef(row['relPlace']['value']), namespace.SKOS.broader, rel.p_luovutetut_alueet))
+
+
+def move_old_towns(g):
+    q = '''
+            PREFIX yso: <http://www.yso.fi/onto/yso/>
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+            SELECT DISTINCT ?relPlace ?note
+            WHERE { 
+	        ?place1 a skos:Concept .
+  	        ?place1 skos:inScheme yso:places .
+  		    ?place1 skos:note ?note .
+  		    ?relPlace skos:exactMatch ?place1 .
+  		    FILTER(lang(?note) = 'fi') .
+            }'''
+
+    response = requests.post('http://localhost:3030/ds/query',
+                             data={'query': q})
+
+    for row in response.json()['results']['bindings']:
         if row['note']['value'] == "Neuvostoliitolle luovutetun alueen kunta" or \
                 row['note']['value'] == "Neuvostoliitolle luovutetun alueen kaupunki":
             g.add((URIRef(row['relPlace']['value']), namespace.SKOS.broader, rel.p_luovutetut_alueet))
@@ -54,10 +77,10 @@ def ares_given_to_sssr(g):
 
 graph = Graph()
 
-graph.parse('graphs/place_ontology.ttl', format='turtle')
+graph.parse('graphs/no_hierarchy_place_ontology.ttl', format='turtle')
 
 first_level_broader(graph)
 
-ares_given_to_sssr(graph)
+ares_given_to_ussr(graph)
 
-graph.serialize('graphs/place_ontology2.ttl', format='turtle')
+graph.serialize('graphs/place_ontology3.ttl', format='turtle')
--- place_ontology.py
+++ place_ontology.py
@@ -25,45 +25,6 @@ def generate_places(r_g, w_g):
 
         x=x+1
 
-def first_level_broader(g):
-    q = '''
-        PREFIX yso: <http://www.yso.fi/onto/yso/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX rel: <http://ldf.fi/relsearch/>
-
-        SELECT DISTINCT ?relPlace ?broaderPlace
-        WHERE { 
-	    ?place1 a skos:Concept .
-  	    ?place1 skos:inScheme yso:places .
-  	    ?relPlace a rel:Place .
-  	    ?place1 skos:broader ?place2 .
-  	    ?relPlace skos:exactMatch ?place1 .
-  	    ?broaderPlace skos:exactMatch ?place2 .	    
-        }'''
-
-    response = requests.post('http://localhost:3030/ds/query',
-                             data={'query': q})
-
-    for row in response.json()['results']['bindings']:
-        g.add((URIRef(row['relPlace']['value']), namespace.SKOS.broader, URIRef(row['broaderPlace']['value'])))
-
-def remove_yso(g):
-    q = g.query('''
-        PREFIX yso: <http://www.yso.fi/onto/yso/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX rel: <http://ldf.fi/relsearch/>
-
-        SELECT DISTINCT ?place1
-        WHERE { 
-	    ?place1 a skos:Concept .
-  	    ?place1 skos:inScheme yso:places .    
-        }''')
-
-    response = requests.post('http://localhost:3030/ds/query',
-                             data={'query': q})
-
-    for row in response.json()['results']['bindings']:
-        g.add((URIRef(row['']), namespace.RDF.type, rel.Place))
 
 r_graph = Graph()
 w_graph = Graph()
@@ -72,7 +33,7 @@ r_graph.parse('graphs/yso-paikat-skos.ttl', format='turtle')
 
 generate_places(r_graph, w_graph)
 
-w_graph.serialize('graphs/place_ontology.ttl', format='turtle')
+w_graph.serialize('graphs/no_hierarchy_place_ontology.ttl', format='turtle')
 
 #graph1 = Graph()
 
